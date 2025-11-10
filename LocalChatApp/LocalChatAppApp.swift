ec48f514afc679029d04c9584eb858735463436d
Greg <ghard123@gmail.com>
2025-11-11 10:32:22 +1100

    Initial Commit

---
+++ LocalChatApp/LocalChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalChatAppApp.swift
+//  LocalChatApp
+//
+//  Created by Greg on 11/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct LocalChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
